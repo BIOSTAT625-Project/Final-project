13ce18dd19f4f44a16199dd366a27b41bf412b2d
ConchaEspina <claireq99@gmail.com>
2022-12-17 01:10:50 -0500

    Update: app and data_clean
    Add the complete framework of app (app.R) and data cleaning script (data_clean.R).

--- app.R
+++ app.R
@@ -1,50 +1,121 @@
 # Working directory: ~/Desktop/BIOSTAT 625 - Computing with Big Data/Final Project
-setwd("~/Desktop/BIOSTAT 625 - Computing with Big Data/Final Project")
+# setwd("~/Desktop/BIOSTAT 625 - Computing with Big Data/Final Project")
+setwd("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/")
 
 library(shiny)
 library(bslib)
 library(dplyr)
 library(ggplot2)
-library(rdrop2)
+library(tools)
 library(readxl)
 library(usmap)
+library(rsconnect)
+library(rcartocolor)
+library(showtext)
+library(lubridate)
+
+# setAccountInfo(name = "conchaespina",
+#                token = "12CE5D455F897C2F0EF8017E62295CE9",
+#                secret = "kWvE26n6im+fK7rnDyyU8L3o9Bq6joXb1eSe6Trz")
+# deployApp("")
 
 state_list <- read.csv("state.csv")
+keywords_list <- c("depression", "depressed", "empty", "fatigue", 
+                   "feeling sad", "guilty", "insomnia", "suicide")
+aux_data_list <- c("weather", "AQI", "income", "education", "sunlight", "unemployment rate", "median age")
 
-token <- drop_auth()
-drop_read_excel <- function(file, skip = 0, dest = tempdir(), dtoken = rdrop2:::get_dropbox_token(), ...) {
-  localfile = paste0(dest, "/", basename(file))
-  drop_download(path = file, local_path = localfile, overwrite = TRUE, dtoken = dtoken)
-  read_excel(path = localfile, skip = skip, ...)
-}
+dat <- read.csv("Clean_data/google_trends_all.csv") %>% 
+  mutate(week = as.Date(week)) %>% 
+  filter(complete.cases(.))
 
-create_keyword_data <- function(keyword, root_path = "/final project/bio625_final_project_rawdata/") {
-  df <- drop_read_excel(file = paste0(root_path, 
-                                      "biostat625 final raw-google trends/biostat625 final raw/", 
-                                      keyword, " (us) – google trends export – glimpse.xlsx"), 
-                        skip = 6) %>% 
-    mutate(Week = as.Date(Week), Region = "US")
+get_aux <- function(aux_type, time_start, time_end, var_name = NULL) {
   
-  for (state in state_list$code) {
-    df_state <- drop_read_excel(file = paste0(root_path, keyword, " by state (recent 5 years)/", 
-                                              keyword, " (us-", tolower(state), 
-                                              ") – google trends export – glimpse.xlsx"), 
-                                skip = 6) %>% 
-      mutate(Week = as.Date(Week), Region = state)
-    df <- rbind(df, df_state)
+  if (aux_type == "weather") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/weather.csv") %>% 
+      select("name", "datetime", all_of(var_name)) %>% 
+      filter((datetime >= time_start) & (datetime <= time_end)) %>% 
+      group_by(name) %>% 
+      summarize(avg = as.character(round(mean(eval(parse(text = var_name)), na.rm = TRUE), 
+                                         digits = 2))) 
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("name" = "full"))
   }
   
-  return(df)
+  else if (aux_type == "AQI") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/aqi_complete.csv") %>% 
+      select(state, date_local, Mean_aqi) %>% 
+      mutate(date_local = as.Date(date_local, format = "%m/%d/%y")) %>% 
+      filter((date_local >= time_start) & (date_local <= time_end)) %>% 
+      group_by(state) %>% 
+      summarize(avg = as.character(round(mean(Mean_aqi, na.rm = TRUE), 
+                                         digits = 2)))
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("state" = "full"))
+  }
+  
+  else if (aux_type == "income") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/personal_income_by_state.csv") %>% 
+      tidyr::pivot_longer(cols = !DATE) %>% 
+      filter((DATE >= year(ymd(time_start))) & (DATE <= year(ymd(time_end)))) %>% 
+      group_by(name) %>% 
+      summarize(avg = as.character(round(mean(value, na.rm = TRUE), 
+                                         digits = 2)))
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("name" = "abbr"))
+  }
+  
+  else if (aux_type == "education") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/EducationReport 2016-2020 (average).csv") %>% 
+      mutate(avg = round(eval(parse(text = var_name)), digits = 2))
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("State" = "full"))
+  }
+  
+  else if (aux_type == "sunlight") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/Daily Sunlight (2004-2011).csv") %>% 
+      group_by(State) %>% 
+      summarize(avg = as.character(round(mean(Avg.Daily.Sunlight, na.rm = TRUE), digits = 2)))
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("State" = "full"))
+  }
+  
+  else if (aux_type == "unemployment rate") {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/STATE-UI-RATES 2009-2022.csv") %>% 
+      mutate(Month = sapply(X = Month, 
+                            FUN = function(x) {which(month.abb == substr(x, start = 1, stop = 3))})) %>% 
+      mutate(datetime = mapply(FUN = make_date, year = Year, month = Month)) %>% 
+      mutate(datetime = as_date(datetime)) %>% 
+      select(-Year, -Month) %>% 
+      filter((datetime >= time_start) & (datetime <= time_end)) %>% 
+      tidyr::pivot_longer(cols = !datetime) %>% 
+      mutate(name = sapply(X = name, 
+                           FUN = function(x) {paste(strsplit(x = x, split = "\\.")[[1]], 
+                                                    collapse = " ")})) %>% 
+      group_by(name) %>% 
+      summarize(avg = as.character(round(mean(value, na.rm = TRUE), digits = 2)))
+    
+    aux <- aux %>% 
+      right_join(usmapdata::centroid_labels("states"), by = c("name" = "full"))
+  }
+  
+  else {
+    aux <- read.csv("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/Clean_data/Median Age by state 2022.csv") %>% 
+      rename(avg = Median.Age) %>%
+      right_join(usmapdata::centroid_labels("states"), by = c("State" = "abbr"))
+  }
+  
+  
+  return(aux)
 }
 
-depression <- create_keyword_data(keyword = "Depression")
-
-
+font_add_google(name = "News Cycle")
+showtext_auto()
 
 # Define UI for app ----
 ui <- fluidPage(
   # Theme
-  theme = bs_theme(bootswatch = "journal"),
+  theme = bs_theme(bootswatch = "journal", 
+                   base_font = "News Cycle"),
   
   # App title ----
   titlePanel("Population Depression Data from Google Trends"),
@@ -68,10 +139,17 @@ ui <- fluidPage(
       ),
       
       # Settings ----
+      selectInput(
+        inputId = "keyword", 
+        label = "Keyword", 
+        choices = split(x = keywords_list, f = toTitleCase(keywords_list)), 
+        selected = "depression"
+      ),
+      
       dateRangeInput(
         inputId = "time_range", 
         label = "Time range", 
-        start = "2022-01-01", 
+        start = "2020-01-01", 
         end = "2022-10-31", 
         min = "2004-01-01"
       ),
@@ -87,6 +165,42 @@ ui <- fluidPage(
         )
       ),
       
+      conditionalPanel(
+        condition = "input.plot_type == 'region'",
+        selectInput(
+          inputId = "indicator", 
+          label = "Other indicators", 
+          choices = c(`None` = "None", 
+                      split(x = aux_data_list, f = toTitleCase(aux_data_list))), 
+          selected = "None"
+        )
+      ),
+      
+      conditionalPanel(
+        condition = "input.indicator == 'weather'",
+        selectInput(
+          inputId = "weather", 
+          label = "Weather", 
+          choices = list(`Average temperature` = "temp", 
+                         `Average feels like temperature` = "feelslike", 
+                         `Average dew point` = "dew", 
+                         `Average humidity` = "humidity", 
+                         `Average precipitation` = "precip", 
+                         `Average windspeed` = "windspeed", 
+                         `Average visibility` = "visibility")
+        )
+      ),
+      
+      conditionalPanel(
+        condition = "input.indicator == 'education'",
+        selectInput(
+          inputId = "education", 
+          label = "Education", 
+          choices = list(`Complete college` = "Complete_college",	
+                         `Complete high school only` = "Complete_high_school_only", 
+                         `Not complete high school` = "Not_complete_high_school")
+        )
+      ),
       
       conditionalPanel(
         condition = "input.time_range[1] > '2022-11-20'", 
@@ -96,8 +210,9 @@ ui <- fluidPage(
           choices = list(`None` = "None"), 
           selected = "None"
         )
-      )
+      ),
       
+      width = 3
     ),
     
     # Main panel for displaying outputs ----
@@ -106,13 +221,13 @@ ui <- fluidPage(
       # Output: Line graph ----
       conditionalPanel(
         condition = "input.plot_type == 'time'", 
-        plotOutput(outputId = "line_graph")
+        plotly::plotlyOutput(outputId = "line_graph")
       ),
       
       # Output: Map ----
       conditionalPanel(
         condition = "input.plot_type == 'region'", 
-        plotOutput(outputId = "map")
+        plotly::plotlyOutput(outputId = "map", height = 500)
       )
       
     )
@@ -122,32 +237,82 @@ ui <- fluidPage(
 # Define server logic required to draw plots ----
 server <- function(input, output) {
   
-  output$line_graph <- renderPlot({
-    
-    depression %>% 
-      filter((Week >= input$time_range[1]) & (Week <= input$time_range[2])) %>% 
-      filter(Region == input$region) %>% 
-      ggplot(mapping = aes(x = Week, y = `Search Volume (Absolute)`)) + 
-      geom_line() + 
-      theme_bw(base_size = 12)
-    
-  })
+  output$line_graph <- plotly::renderPlotly(
+    expr = {
+      dat %>% 
+        filter((week >= input$time_range[1]) & (week <= input$time_range[2])) %>% 
+        filter(keyword == input$keyword) %>% 
+        filter(region == input$region) %>% 
+        ggplot(data = ., mapping = aes(x = week, y = absolute, group = 1)) + 
+        geom_line(color = "#63a6a0", size = 1) + 
+        labs(x = "Week", y = "Absolute search volume") + 
+        scale_x_date(breaks = seq.Date(from = input$time_range[1], 
+                                       to = input$time_range[2], 
+                                       length.out = 20)) + 
+        theme_bw(base_size = 13) + 
+        theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), 
+              text = element_text(family = "News Cycle"), 
+              plot.margin = margin(0, 0, 0, 1, "cm"))
+    }
+  )
   
-  output$map <- renderPlot({
-    
-    depression %>% 
-      filter((Week >= input$time_range[1]) & (Week <= input$time_range[2])) %>% 
-      group_by(Region) %>% 
-      summarize(abs_volume = sum(`Search Volume (Absolute)`)) %>% 
-      left_join(state_list, by = c("Region" = "code")) %>% 
-      plot_usmap(data = ., values = "abs_volume", regions = "states") + 
-      scale_fill_viridis_c(option = "magma", begin = 0.1, end = 0.9, direction = -1) + 
-      theme_bw() + 
-      theme(axis.title = element_blank(), 
-            axis.text = element_blank(), 
-            axis.ticks = element_blank())
-    
-  })
+  output$map <- plotly::renderPlotly(
+    expr = {
+      # Dataset to create basic map
+      gt_data <- dat %>% 
+        filter((week >= input$time_range[1]) & (week <= input$time_range[2])) %>% 
+        filter(keyword == input$keyword) %>% 
+        filter(region != "US") %>% 
+        group_by(region) %>% 
+        summarize(absolute = sum(absolute)) %>% 
+        left_join(state_list, by = c("region" = "code"))
+      
+      # Add prediction
+      
+      # Basic map
+      map_plot <- plot_usmap(data = gt_data, values = "absolute", regions = "states", 
+                             theme = theme_classic(), color = "#bcbcbc", size = 0.6) + 
+        scale_fill_carto_c(palette = "Mint", name = "Absolute\nsearch\nvolume") + 
+        theme_bw(base_size = 13) + 
+        theme(axis.title = element_blank(), 
+              axis.text = element_blank(), 
+              axis.ticks = element_blank(), 
+              text = element_text(family = "News Cycle"), 
+              plot.margin = margin(0, 0, 0, 0, "cm")) 
+      
+      # Add labels for auxiliary indicators
+      if (input$indicator != "None") {
+        
+        if (input$indicator == "weather") {
+          aux <- get_aux(aux_type = "weather", 
+                         time_start = as.character(input$time_range[1]), 
+                         time_end = as.character(input$time_range[2]), 
+                         var_name = input$weather)
+        }
+        
+        else if (input$indicator == "education") {
+          aux <- get_aux(aux_type = "education", 
+                         time_start = as.character(input$time_range[1]), 
+                         time_end = as.character(input$time_range[2]), 
+                         var_name = input$education)
+        }
+        
+        else {
+          aux <- get_aux(aux_type = input$indicator, 
+                         time_start = as.character(input$time_range[1]), 
+                         time_end = as.character(input$time_range[2]))
+        }
+        
+        map_plot <- map_plot + 
+          geom_text(mapping = aes(x = x, y = y, label = avg), data = aux, 
+                    inherit.aes = FALSE, size = 3)
+        
+      }
+      
+      plotly::ggplotly(p = map_plot, tooltip = c("fill"))
+      
+    }
+  )
   
 }
 
---
+++ data_clean.R
@@ -0,0 +1,139 @@
+# Working directory: ~/Dropbox (University of Michigan)/Final project
+setwd("~/Dropbox (University of Michigan)/Final project/BIO625_final_project_Rawdata/")
+
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(tools)
+library(lme4)
+library(lubridate)
+
+state_list <- read.csv("state.csv")
+
+# ===== GOOGLE TRENDS DATA =====
+
+keywords_list <- c("depression", "depressed", "empty", "fatigue", 
+                   "feeling sad", "guilty", "insomnia", "suicide")
+
+relative_convert <- function(df_late, df_early) {
+  df <- df_early %>%
+    full_join(df_late,
+              by = c("week" = "week", "keyword" = "keyword", "region" = "region"),
+              suffix = c("_early", ""))
+  
+  lm_mods <- summary(lmList(formula = relative ~ relative_early - 1 | region,
+                            data = df))$coefficients[, "Estimate", ] %>%
+    data.frame(slope = .) %>% 
+    mutate(region = rownames(.), 
+           slope = replace_na(data = slope, replace = 1))
+  
+  df <- df %>%
+    left_join(lm_mods, by = c("region" = "region")) %>%
+    mutate(relative = ifelse(test = is.na(relative),
+                             yes = round(x = slope * relative_early, digits = 0),
+                             no = relative)) %>% 
+    select(-relative_early, -slope) %>% 
+    arrange(region, week)
+  return(df)
+}
+
+combine_vol <- function(keyword) {
+  state_1722 <- read.csv(paste0(keyword, "_by_state.csv")) %>% 
+    rename(week = Week, relative = Relative, absolute = Absolute, region = State) %>% 
+    mutate(week = ymd(week), keyword = keyword, 
+           region = gsub(pattern = "US-", replacement = "", x = region)) %>% 
+    as_tibble()
+  
+  for (i in 4:1) {
+    state_early <- read.csv(paste0("Clean_data/", 
+                                   keyword, "_", sprintf("%02d", 4 * i), "_", 
+                                   sprintf("%02d", 4 * (i + 1)), ".csv")) %>% 
+      filter(geo != "US") %>% 
+      select(date, hits, keyword, geo) %>% 
+      rename(week = date, relative = hits, region = geo) %>% 
+      mutate(week = mdy(week), 
+             region = gsub(pattern = "US-", replacement = "", x = region)) %>% 
+      as_tibble()
+    
+    state_1722 <- relative_convert(df_late = state_1722, df_early = state_early)
+  }
+  
+  return(state_1722)
+}
+
+absolute_convert <- function(df) {
+  lm_mods <- summary(lmList(formula = absolute ~ relative - 1 | region,
+                            data = df))$coefficients[, "Estimate", ] %>%
+    data.frame(slope = .) %>%
+    mutate(region = rownames(.), 
+           slope = replace_na(data = slope, replace = 1))
+  
+  df <- df %>%
+    left_join(lm_mods, by = c("region" = "region")) %>%
+    mutate(absolute = ifelse(test = is.na(absolute),
+                             yes = round(x = slope * relative, digits = 0),
+                             no = absolute)) %>%
+    select(-relative, -slope) %>% 
+    group_by(region) %>% 
+    distinct() %>% 
+    ungroup() %>% 
+    arrange(region, week)
+  
+  return(df)
+}
+
+create_keyword_data <- function(keyword) {
+  # Search volume of US
+  us_data <- read.csv(paste0("Clean_data/", toTitleCase(keyword), "_absolute.csv")) %>% 
+    rename(week = Week, absolute = Absolute_mean) %>% 
+    mutate(week = ymd(week), absolute = round(absolute, digits = 0), keyword = keyword, region = "US")
+  
+  # Search volume of each state
+  state_data <- combine_vol(keyword = keyword) %>% 
+    absolute_convert()
+  
+  # Combine two datasets
+  return(rbind(us_data, state_data))
+}
+
+combine_all_trends <- function() {
+  df <- data.frame()
+  for (kw in keywords_list) {
+    df <- rbind(df, create_keyword_data(kw))
+  }
+  
+  write.csv(df, "Clean_data/google_trends_all.csv")
+}
+
+# Usage: (1) Change the path to your Dropbox folder using setwd(); (2) Run "dat <- combine_all()".
+
+# ===== WEATHER DATA =====
+
+combine_weather <- function() {
+  us_weather <- read.csv("Clean_data/US Weather 2004-01-01 to 2022-12-01.csv") %>% 
+    mutate(datetime = as.Date(datetime, tryFormats = c("%m/%d/%y", "%Y-%m-%d", "%Y/%m/%d")))
+  for (st in state_list$state) {
+    state_weather <- read.csv(paste0("Clean_data/", st, 
+                                     " Weather 2004-01-01 to 2022-12-01.csv")) %>% 
+      mutate(datetime = as.Date(datetime, tryFormats = c("%m/%d/%y", "%Y-%m-%d", "%Y/%m/%d")))
+    us_weather <- rbind(us_weather, state_weather)
+  }
+  
+  write.csv(us_weather, "Clean_data/weather.csv")
+  #return(us_weather)
+}
+
+# ===== 
+
+
+
+
+
+
+
+
+
+
+
+
+
